4bd956226c3eaff93e30eed70f90aebc65670b17
Stephanie La Raja <stephanielaraja@gmail.com>
2021-04-18 11:43:58 -0400

    commit tree 5e7188b13c57fd4fdd068f35dbbd36562b81b3e7

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+
+    def current_user(session)
+        #use the user_id from the session hash to find the user in the database and return that user.
+        @user = User.find_by_id(session[:user_id])
+    end
+    
+    def is_logged_in?(session)
+        #return true if the user_id is in the session hash and false if not.
+        !!session[:user_id]
+    end
+
+
 end
